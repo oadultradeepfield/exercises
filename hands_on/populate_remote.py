ba2ef97b074805ddbccfa6423d354e96b07897cc
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-10-26 12:31:58 +0800

    chore: Minor improvement in abstraction

--- hands_on/populate_remote.py
+++ hands_on/populate_remote.py
@@ -13,7 +13,7 @@ REPO_NAME = "gitmastery-things"
 
 def download(verbose: bool):
     _setup_local_repository(verbose)
-    _setup_remote_repository(verbose)
+    _ensure_clean_repository(verbose)
     _link_repositories(verbose)
 
 
@@ -25,14 +25,22 @@ def _setup_local_repository(verbose: bool):
     _add_additional_files(verbose)
 
 
-def _setup_remote_repository(verbose: bool):
-    username = _get_github_username(verbose)
-    _ensure_clean_repository(verbose)
+def _ensure_clean_repository(verbose: bool):
+    repo_check = subprocess.run(
+        ["gh", "repo", "view", _get_full_repo_name(verbose)],
+        capture_output=True,
+        text=True
+    )
+
+    if repo_check.returncode == 0:
+        run_command(["gh", "repo", "delete", REPO_NAME, "--yes"], verbose)
+
+    run_command(["gh", "repo", "create", REPO_NAME, "--public"], verbose)
 
 
 def _link_repositories(verbose: bool):
-    remote_url = _build_remote_url(verbose)
-    add_origin(remote_url, verbose)
+    full_repo_name = _get_full_repo_name(verbose)
+    add_origin(f"https://github.com/{full_repo_name}", verbose)
 
 
 def _initialize_workspace():
@@ -68,19 +76,6 @@ def _add_additional_files(verbose: bool):
     commit("Add colours.txt, shapes.txt", verbose)
 
 
-def _get_github_username(verbose: bool) -> str:
-    return run_command(["gh", "api", "user", "-q", ".login"], verbose).strip()
-
-
-def _ensure_clean_repository(verbose: bool):
-    result = subprocess.run(["gh", "api", "user", "-q", ".login"], capture_output=True, text=True)
-
-    if result.returncode == 0:
-        run_command(["gh", "repo", "delete", REPO_NAME, "--yes"], verbose)
-
-    run_command(["gh", "repo", "create", REPO_NAME, "--public"], verbose)
-
-
-def _build_remote_url(verbose: bool) -> str:
-    username = _get_github_username(verbose)
-    return f"https://github.com/{username}/{REPO_NAME}.git"
+def _get_full_repo_name(verbose: bool) -> str:
+    username = run_command(["gh", "api", "user", "-q", ".login"], verbose).strip()
+    return f"{username}/{REPO_NAME}"
