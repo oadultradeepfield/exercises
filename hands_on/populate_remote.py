5e001c3e2724dcce5f986b5a397919b9bb030096
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-10-26 21:10:00 +0800

    chore: Improve code formatting

--- hands_on/populate_remote.py
+++ hands_on/populate_remote.py
@@ -27,22 +27,21 @@ def _setup_local_repository(verbose: bool):
     )
     add(["fruits.txt"], verbose)
 
-
     append_to_file("fruits.txt", """
         figs
         """,
-                   )
+    )
     add(["fruits.txt"], verbose)
     commit("Insert figs into fruits.txt", verbose)
 
     create_or_update_file("colours.txt", """
         a file for colours 
         """,
-                          )
+    )
     create_or_update_file("shapes.txt", """
         a file for shapes 
         """,
-                          )
+    )
     add(["colours.txt", "shapes.txt"], verbose)
     commit("Add colours.txt, shapes.txt", verbose)
 
