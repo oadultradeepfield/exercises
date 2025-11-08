d92ce9fac8e08a95e38e7ea1724f4131a991a85f
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-11-09 07:57:23 +0800

    feat: Implement the download script

--- merge_squash/download.py
+++ merge_squash/download.py
@@ -1,8 +1,29 @@
-from exercise_utils.cli import run_command
+from exercise_utils.file import create_or_update_file
+from exercise_utils.git import add, commit, checkout
 from exercise_utils.gitmastery import create_start_tag
-
-__resources__ = {}
 
 
 def setup(verbose: bool = False):
     create_start_tag(verbose)
+
+    create_or_update_file("joey.txt", "Matt LeBlanc")
+    add(["joey.txt"], verbose)
+    commit("Add Joey", verbose)
+
+    create_or_update_file("phoebe.txt", "Lisa Kudrow")
+    add(["phoebe.txt"], verbose)
+    commit("Add Phoebe", verbose)
+
+    checkout("supporting", True, verbose)
+    create_or_update_file("mike.txt", "Paul Rudd")
+    add(["mike.txt"], verbose)
+    commit("Add Mike", verbose)
+
+    create_or_update_file("janice.txt", "Maggie Wheeler")
+    add(["janice.txt"], verbose)
+    commit("Add Janice", verbose)
+
+    checkout("main", False, verbose)
+    create_or_update_file("ross.txt", "David Schwimmer")
+    add(["ross.txt"], verbose)
+    commit("Add Ross", verbose)
