7ee2c1317fb96c99f1c6543815f9c03b220a77fb
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-11-09 10:46:20 +0800

    tests: Add unit-tests for merge squash

--- merge_squash/tests/test_verify.py
+++ merge_squash/tests/test_verify.py
@@ -1,6 +1,14 @@
-from git_autograder import GitAutograderTestLoader
+from git_autograder import GitAutograderStatus, GitAutograderTestLoader, assert_output
 
-from ..verify import verify
+from ..verify import (
+    FAST_FORWARD_MERGE,
+    NO_SQUASH_MERGE,
+    NOT_ON_MAIN,
+    REGULAR_MERGE,
+    RESET_MESSAGE,
+    UNCOMMITTED_CHANGES,
+    verify,
+)
 
 REPOSITORY_NAME = "merge-squash"
 
@@ -8,5 +16,33 @@ loader = GitAutograderTestLoader(__file__, REPOSITORY_NAME, verify)
 
 
 def test_base():
-    with loader.load("specs/base.yml", "start"):
-        pass
+    with loader.load("specs/base.yml", "start") as output:
+        assert_output(output, GitAutograderStatus.SUCCESSFUL)
+
+
+def test_no_merge():
+    with loader.load("specs/no_merge.yml", "start") as output:
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [NO_SQUASH_MERGE, RESET_MESSAGE],
+        )
+
+
+def test_regular_merge():
+    with loader.load("specs/regular_merge.yml", "start") as output:
+        assert_output(
+            output,
+            GitAutograderStatus.UNSUCCESSFUL,
+            [REGULAR_MERGE, RESET_MESSAGE],
+        )
+
+
+def test_uncommitted():
+    with loader.load("specs/uncommitted.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [UNCOMMITTED_CHANGES])
+
+
+def test_not_main():
+    with loader.load("specs/not_main.yml") as output:
+        assert_output(output, GitAutograderStatus.UNSUCCESSFUL, [NOT_ON_MAIN])
