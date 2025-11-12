363c914b03b43c9f96dae4cc6b29fca113a9f290
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-12 15:36:45 +0800

    [test] Testing change for app

---
+++ hands_on/test.py
@@ -0,0 +1,9 @@
+from exercise_utils.cli import run_command
+from exercise_utils.gitmastery import create_start_tag
+
+__requires_git__ = True
+__requires_github__ = False
+
+
+def download(verbose: bool):
+    raise Exception("this should not work")
